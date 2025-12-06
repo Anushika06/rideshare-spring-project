122b70936b68883b61cae3c1ecef0638f2525d19
Anushika/06 <anushika.24bcs10344@sst.scaler.com>
2025-12-06 20:08:41 +0530

    changed package name

--- project/src/main/java/com/anushika/project/exception/BadRequestException.java
+++
@@ -1,8 +0,0 @@
-package com.anushika.project.exception;
-
-public class BadRequestException extends RuntimeException {
-
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
--- project/src/main/java/com/anushika/project/exception/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.anushika.project.exception;
-
-public class NotFoundException extends RuntimeException {
-
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
