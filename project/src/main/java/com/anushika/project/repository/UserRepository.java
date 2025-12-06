35703729ae419e64fdf69f22758f83117da2337a
Anushika/06 <anushika.24bcs10344@sst.scaler.com>
2025-12-06 19:55:50 +0530

    changed package name

---
+++ project/src/main/java/com/anushika/project/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.anushika.project.repository;
+
+import com.anushika.project.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+}
