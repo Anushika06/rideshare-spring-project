a3c1851b1267af9b8fbd6eb287c900dd888449de
Anushika/06 <anushika.24bcs10344@sst.scaler.com>
2025-12-06 20:15:08 +0530

    changed package name

---
+++ project/src/main/java/com/anushika/project/repository/RideRepository.java
@@ -0,0 +1,13 @@
+package com.anushika.project.repository;
+
+import com.anushika.project.model.Ride;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface RideRepository extends MongoRepository<Ride, String> {
+
+    List<Ride> findByStatus(String status);
+
+    List<Ride> findByUserId(String userId);
+}
